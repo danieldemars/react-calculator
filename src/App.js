d782733d08de9135b8a1c5d80db8908f246e0510
Daniel DeMars <demarsdaniel02@gmail.com>
2024-04-12 15:14:23 -0500

    Practice react app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import "./calculator.css";
 
 function CalcButton({ text, onButtonClick }) {
@@ -16,13 +17,13 @@ function ButtonDisplay( {handleClick} ) {
         <CalcButton text={"Clear"} onButtonClick={() => handleClick("clear")} />
         <CalcButton text={"+/-"} onButtonClick={() => handleClick("+/-")} />
         <CalcButton text={"%"} onButtonClick={() => handleClick("%")} />
-        <CalcButton text={"÷"} onButtonClick={() => handleClick("divide")} />
+        <CalcButton text={"÷"} onButtonClick={() => handleClick("/")} />
       </div>
       <div className="calc-row">
         <CalcButton text={"7"} onButtonClick={() => handleClick(7)} />
         <CalcButton text={"8"} onButtonClick={() => handleClick(8)} />
         <CalcButton text={"9"} onButtonClick={() => handleClick(9)} />
-        <CalcButton text={"X"} onButtonClick={() => handleClick("X")} />
+        <CalcButton text={"X"} onButtonClick={() => handleClick("*")} />
       </div>
       <div className="calc-row">
         <CalcButton text={"4"} onButtonClick={() => handleClick(4)} />
@@ -48,57 +49,123 @@ function ButtonDisplay( {handleClick} ) {
 export default function Calculator() {
   const [output, setOutput] = useState(""); 
   const [stack, setStack] = useState([]);
+  const [currentOperation, setCurrentOperation] = useState([]);
+  const [equalsPressed, setEqualsPressed] = useState(false);
 
+  // updates the output when the stack changes
+  useEffect(() => {
+    setOutput(getOutputNumber());
+  }, [stack]); 
+
+  // operation gets performed when equals is pressed or if user enters more than one operator 
+  useEffect(() => {
+    if (equalsPressed && currentOperation.length === 3){
+      performOperation();
+      setEqualsPressed(false);
+      console.log("Operation performed");
+    }
+  }, [equalsPressed, currentOperation]); 
+  
+
+  // handles the click event of the buttons
   function handleClick(value){
     if (value === 1) {
-      setStack(prevStack => [...prevStack, 1]); 
-      setOutput(1);
+      setStack((prevStack) => [...prevStack, 1]); 
       return;
     } else if (value === 2) {
-      setStack(prevStack => [...prevStack, 2]);
-      setOutput(2);
+      setStack((prevStack) => [...prevStack, 2]);
       return;
     } else if (value === 3) {
       setStack((prevStack) => [...prevStack, 3]);
-      setOutput(3);
       return;
     } else if (value === 4) {
       setStack((prevStack) => [...prevStack, 4]);
-      setOutput(4);
       return;
     } else if (value === 5) {
       setStack((prevStack) => [...prevStack, 5]);
-      setOutput(5);
       return;
     } else if (value === 6) {
       setStack((prevStack) => [...prevStack, 6]);
-      setOutput(6);
       return;
     } else if (value === 7) {
       setStack((prevStack) => [...prevStack, 7]);
-      setOutput(7);
       return;
     } else if (value === 8) {
       setStack((prevStack) => [...prevStack, 8]);
-      setOutput(8);
       return;
     } else if (value === 9) {
       setStack((prevStack) => [...prevStack, 9]);
-      setOutput(9);
       return;
     } else if (value === 0) {
-      setStack((prevStack) => [...prevStack, 0]);;
+      setStack((prevStack) => [...prevStack, 0]);
+      return;
+    } else if (value === "clear"){
+      setStack([]);
+      setCurrentOperation([]);
       setOutput(0);
       return;
+    } else if (value === "="){
+      setCurrentOperation(prevOperation => [...prevOperation, getOutputNumber()]);
+      setEqualsPressed(true);
+      console.log("Equals Pressed");
+      return;
+    } else if (value === "+"){
+      addOperator("+");
+      return;
+    } 
+  }
+
+  // returns the number from the stack
+  function getOutputNumber(){
+    if (stack.length === 0) {
+      return 0;
     } else {
-      console.log(stack);
+      let number = parseInt(stack.join(''));
+      return number; 
     }
+  }
+
+  // adds operator to currentOperation and clears the stack whenever the user clicks an operator
+  function addOperator(operator){
+    let operators = ["+", "-", "*", "/"]; 
+
+    // checks if an operator is already in the currentOperation, if so then complete the operation
+    if (operators.some(operator => currentOperation.includes(operator))){ 
+      // TODO: complete operation
+    } else {
+      setCurrentOperation([...currentOperation, getOutputNumber(), operator]);
+      setStack([]);
+    }
+  }
+
+  function performOperation(){
+    // uses object lookup to decide which operation to perform based on the operator
+    let operators = {
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "*": (a, b) => a * b,
+      "/": (a, b) => a / b,
+    };
+
+    // completes the operation
+    console.log("Current Operation prior to operation: ", currentOperation);
+    let operator = currentOperation[1];
+    let result = operators[operator](currentOperation[0], currentOperation[2]);
+    console.log(currentOperation[0], operator, currentOperation[2], "=", result); 
+
+    // updates currentOperation
+    setCurrentOperation([result]);
+
+    // updates the output for the calculator
+    setOutput(result);
   }
   
   return (
     <>
       <Output displayValue={output}/>
       <ButtonDisplay handleClick={handleClick}/>
+      <p>Stack: {stack}</p>
+      <p>Current Operation: {currentOperation}</p>
     </>
   );
 }
